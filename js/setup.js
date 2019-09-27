a2f7f31051e06a59c6faac60b8bd4fdb1c77b170
Sergei Pavlov <unplanetx@gmail.com>
2019-09-27 21:10:50 +1000

    Выполняет задание 7

--- js/setup.js
+++ js/setup.js
@@ -4,7 +4,20 @@ var FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К�
 var SECOND_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 var WIZARD_COUNT = 4;
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+var setupOpenElement = document.querySelector('.setup-open');
+var setupOpenIconElement = document.querySelector('.setup-open-icon');
+var setupCloseElement = document.querySelector('.setup-close');
+var setupUserNameElement = document.querySelector('.setup-user-name');
+var wizardCoatElement = document.querySelector('.setup-wizard .wizard-coat');
+var wizardEyesElement = document.querySelector('.setup-wizard .wizard-eyes');
+var wizardFireballElement = document.querySelector('.setup-fireball-wrap');
+var coatColorInputElement = document.querySelector('.setup-wizard-appearance input[name=coat-color]');
+var eyesColorInputElement = document.querySelector('.setup-wizard-appearance input[name=eyes-color]');
+var fireballColorInputElement = document.querySelector('.setup-fireball-wrap input[name=fireball-color]');
 
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .content
@@ -50,7 +63,64 @@ var renderWizards = function (listWizards) {
   return fragment;
 };
 
+var onWizardCoatElementClick = function () {
+  var randomColor = getRandomElementFromArray(COAT_COLORS);
+  wizardCoatElement.setAttribute('style', 'fill: ' + randomColor);
+  coatColorInputElement.setAttribute('value', randomColor);
+};
+
+var onWizardEyesElementClick = function () {
+  var randomColor = getRandomElementFromArray(EYES_COLORS);
+  wizardEyesElement.setAttribute('style', 'fill: ' + randomColor);
+  eyesColorInputElement.setAttribute('value', randomColor);
+};
+
+var onWizardFireballElementClick = function () {
+  var randomColor = getRandomElementFromArray(FIREBALL_COLORS);
+  wizardFireballElement.setAttribute('style', 'background-color: ' + randomColor);
+  fireballColorInputElement.setAttribute('value', randomColor);
+};
+
+var openSetup = function () {
+  userDialog.classList.remove('hidden');
+  setupCloseElement.addEventListener('click', closeSetup);
+  setupCloseElement.addEventListener('keydown', onSetupCloseEnterPress);
+  document.addEventListener('keydown', onSetupEscPress);
+  wizardCoatElement.addEventListener('click', onWizardCoatElementClick);
+  wizardEyesElement.addEventListener('click', onWizardEyesElementClick);
+  wizardFireballElement.addEventListener('click', onWizardFireballElementClick);
+};
+
+var closeSetup = function () {
+  userDialog.classList.add('hidden');
+  setupCloseElement.removeEventListener('click', closeSetup);
+  setupCloseElement.removeEventListener('keydown', onSetupCloseEnterPress);
+  document.removeEventListener('keydown', onSetupEscPress);
+  wizardCoatElement.removeEventListener('click', onWizardCoatElementClick);
+  wizardEyesElement.removeEventListener('click', onWizardEyesElementClick);
+  wizardFireballElement.removeEventListener('click', onWizardFireballElementClick);
+};
+
+var onSetupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE && evt.target !== setupUserNameElement) {
+    closeSetup();
+  }
+};
+
+var onSetupCloseEnterPress = function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE && evt.target === setupCloseElement) {
+    closeSetup();
+  }
+};
+
 var wizards = getWizards();
 similarListElement.appendChild(renderWizards(wizards));
-userDialog.classList.remove('hidden');
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
+
+setupOpenIconElement.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openSetup();
+  }
+});
+
+setupOpenElement.addEventListener('click', openSetup);
